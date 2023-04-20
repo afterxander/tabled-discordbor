2c3013635e3d1ce77ffffe16abb199c169a7ca07
Afterxander <69757706+afterxander@users.noreply.github.com>
2023-04-20 04:58:35 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,102 +1,78 @@
 const Discord = require('discord.js');
-const client = new Discord.Client(MTA5ODIwNzE0NzEzNDgyNDUwOA.GwqDHG.N-7ywM7izAn3wSuYEeeEuIX7SSuMvzNunDtn6E);
+const client = new Discord.Client();
+const prefix = 't!'; // change this to your preferred prefix
 
+// define a collection to hold the commands
+const commands = new Discord.Collection();
+
+// add a command to the collection
+commands.set('table', {
+  description: 'Displays a formatted table.',
+  usage: 't!table\n| Column 1 | Column 2 |\n|----------|----------|\n| Row 1, Column 1 | Row 1, Column 2 |\n| Row 2, Column 1 | Row 2, Column 2 |'
+});
+
+// listen for the ready event
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-  if (message.content.startsWith('t!table')) {
-    const lines = message.content.split('\n');
-    const options = lines[0].substring(8).trim();
-    const rows = lines.slice(1).map(line => line.split('|').map(cell => cell.trim()));
-    const table = createTable(rows, options);
-    message.channel.send('```' + table + '```');
+// listen for the message event
+client.on('message', async (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'help') {
+    const commandsList = commands.map((cmd) => `**${cmd.usage}** - ${cmd.description}`).join('\n');
+    const embed = new Discord.MessageEmbed().setColor('#0099ff').setTitle('Commands List').setDescription(commandsList);
+    message.channel.send(embed);
+  } else if (command === 'table') {
+    try {
+      const lines = commands.get(command).usage.split('\n');
+      const rows = lines.slice(1).map((line) => line.split('|').map((cell) => cell.trim()));
+      const table = createTable(rows);
+      message.channel.send('`' + table + '`');
+    } catch (error) {
+      console.error(error);
+      message.reply('There was an error processing your request.');
+    }
+  } else {
+    message.channel.send('Invalid command.');
   }
 });
 
-function createTable(rows, options) {
-  const opts = parseOptions(options);
-  const widths = getColumnWidths(rows, opts);
-  const borderChars = getBorderChars(opts.border);
-  let table = '';
-
-  rows.forEach((row, i) => {
-    if (i === 0 || opts.lineOnAllRows) {
-      table += getRowSeparator(widths, borderChars);
+// create a function to create a table from a list of arguments
+function createTable(args) {
+  const columnWidths = {};
+  for (const arg of args) {
+    const width = arg.length;
+    if (!columnWidths[columnWidths.length]) {
+      columnWidths[columnWidths.length] = width;
+    } else {
+      columnWidths[columnWidths.length] = Math.max(columnWidths[columnWidths.length], width);
     }
+  }
 
+  const table = '';
+  for (const row of args) {
     table += '|';
-    row.forEach((cell, j) => {
-      const width = widths[j] + opts.colPaddingLeft + opts.colPaddingRight;
-      const alignedCell = alignCell(cell, width, opts.colAlignment);
-      table += ' '.repeat(opts.colPaddingLeft) + alignedCell + ' '.repeat(opts.colPaddingRight) + '|';
-    });
-
+    for (const cell of row) {
+      const width = columnWidths[columnWidths.length - 1];
+      const alignedCell = alignCell(cell, width);
+      table += ' '.repeat(width - cell.length) + alignedCell + '|';
+    }
     table += '\n';
-  });
-
-  if (rows.length > 1) {
-    table += getRowSeparator(widths, borderChars);
   }
 
   return table;
 }
 
-function parseOptions(optionsString) {
-  const options = {
-    lineOnAllRows: false,
-    singleLine: false,
-    border: 'honeywell',
-    colWidths: {},
-    colPaddingLeft: 0,
-    colPaddingRight: 0,
-    colWordWrap: false,
-    colTruncate: 100,
-    colAlignment: 'left'
-  };
-
-  optionsString.split(/\s+/).forEach(option => {
-    const parts = option.split('=');
-    const key = parts[0].replace('--', '');
-    const value = parts.length > 1 ? parts[1] : true;
-
-    if (key.startsWith('col')) {
-      const colParts = key.split('.');
-      const index = colParts[1];
-      const colKey = colParts[2];
-
-      if (!options.colWidths[index]) {
-        options.colWidths[index] = {};
-      }
-
-      options.colWidths[index][colKey] = value;
-    } else {
-      options[key] = value;
-    }
-  });
-
-  return options;
+// create a function to align a cell to a certain width
+function alignCell(cell, width) {
+  const padding = width - cell.length;
+  return `${cell}${' '.repeat(padding)}`;
 }
 
-function getColumnWidths(rows, opts) {
-  const colWidths = {};
-
-  rows.forEach(row => {
-    row.forEach((cell, i) => {
-      const colOpts = opts.colWidths[i] || {};
-      const contentWidth = cell.length;
-      const width = colOpts.width ? parseInt(colOpts.width) : contentWidth;
-      colWidths[i] = Math.max(colWidths[i] || 0, width);
-    });
-  });
-
-  return Object.values(colWidths);
-}
-
-function getBorderChars(border) {
-  const borders = {
-    honeywell: { h: '-', v: '|', tl: '+', tr: '+', bl: '+', br: '+' },
-    norc: { h: '-', v: '|', tl: '+', tr: '+', bl: '+', br: '+' },
-    ramac: { h: '-', v:
+client.login(MTA5ODIwNzE0NzEzNDgyNDUwOA.GwqDHG.N-7ywM7izAn3wSuYEeeEuIX7SSuMvzNunDtn6E);
     
